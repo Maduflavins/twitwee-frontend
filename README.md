e5dd5c2d848da2c6e8610886fd655920fb8f7c35
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-12-09 15:34:02 +0100

    initial commitment

---
+++ README.md
@@ -0,0 +1,37 @@
+# Picter
+
+> A photo sharing application made using PERN stack
+
+<img src='https://user-images.githubusercontent.com/28904556/83858297-e8448300-a739-11ea-873e-a1dc46189780.png' width='1440' alt='Picter app home page'/>
+
+Check the working demo [here](https://picter-ak.herokuapp.com/)
+
+## Project setup
+
+1. Clone the repo.
+2. Run npm install on both server and client
+
+```javascript
+npm install
+```
+
+3. Inside server folder rename .env.sample to .env and put your database and AWS credentials there.
+4. Import db.sql inside server folder to setup the database.
+
+## Run project
+
+### Front end
+
+```javascript
+npm start // Run the app in development mode.
+npm build // Build the app.
+```
+
+Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
+
+### Front end
+
+```javascript
+npm run dev // Run nodemon dev server.
+npm start // Start the app.
+```
---
+++ src/pages/Register/index.js
@@ -0,0 +1,123 @@
+import React, { useState, useContext } from 'react'
+import { v1 as genId } from 'uuid'
+import Axios from 'axios'
+import { Link, Redirect } from 'react-router-dom'
+
+import Navbar from '../../components/Navbar'
+import { BASE_URL } from '../../config'
+import './style.css'
+
+import { Context as NotificationContext } from '../../context/Notification'
+import { Context as AuthContext } from '../../context/Auth'
+import { ADD_NOTI, LOGIN_USER } from '../../context/actionTypes'
+
+export default () => {
+  const { setNotification } = useContext(NotificationContext)
+  const { setAuthUser } = useContext(AuthContext)
+  const [login, setLogin] = useState(false)
+
+  const [user, setUser] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    username: '',
+    password: ''
+  })
+
+  const updateUser = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value })
+  }
+
+  const setCookie = (name, value) => {
+    const d = new Date()
+    d.setTime(d.getTime() + 3 * 24 * 60 * 60 * 1000)
+    const expires = 'expires=' + d.toUTCString()
+    document.cookie = name + '=' + value + ';' + expires + ';path=/'
+  }
+  var templateParams = {
+    name: user.email.split('@')[0],
+    notes: 'thank you for registering with us',
+    email: user.email
+};
+ 
+
+  // Register user
+  const registerUser = async (event) => {
+    event.preventDefault()
+    try {
+      const resp = await Axios({
+        method: 'POST',
+        url: `${BASE_URL}/auth/register`,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: user
+      })
+      setUser({
+        email: '',
+        username: '',
+        password: ''
+      })
+      setAuthUser({
+        action: LOGIN_USER,
+        data: resp.data.user
+      })
+      setCookie('x-auth-token', resp.data.accessToken)
+      Axios.defaults.headers.common['x-auth-token'] = resp.data.accessToken
+      setLogin(true)
+      emailjs.send('smtp_server', 'template_ac8fe5d', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+    } catch (err) {
+      setNotification({
+        action: ADD_NOTI,
+        data: {
+          id: genId(),
+          message: err.response.data.message,
+          type: 'error',
+          color: 'red'
+        }
+      })
+    }
+  }
+
+  return login ? (
+    <Redirect to='/home' />
+  ) : (
+    <div>
+      <Navbar />
+      <div className='auth-contents'>
+        <div className='form-contents'>
+          <h1 className='heading'>Register account</h1>
+          <form onSubmit={registerUser}>
+            <div className='form-row'>
+              <label>Email Address</label>
+              <input type='email' name='email' onChange={updateUser} value={user.email} required />
+            </div>
+            <div className='form-row'>
+              <label>Password</label>
+              <input
+                type='password'
+                name='password'
+                onChange={updateUser}
+                value={user.password}
+                minLength='8'
+                required
+              />
+            </div>
+            <div className='form-row'>
+              <button>Register</button>
+            </div>
+          </form>
+          <p className='footer-text'>
+            Already have an account? <Link to='/'>Login here</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
